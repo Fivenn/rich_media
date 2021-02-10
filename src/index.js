2ff977d48e86dafced6a597fc45583717717c30d
Nicolas SUEUR <nsueur29@gmail.com>
2021-02-10 09:45:23 +0100

    added leaflet map

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import { VideoPlayer } from "./Components/VideoPlayer"
+import { Map } from "./Components/Map"
 
 ReactDOM.render(
-  <VideoPlayer />,
+  <div>
+    <VideoPlayer/>
+    <Map/>
+  </div>,
   document.getElementById("root")
 )
