428e2bf718f7e1d5e50edba30162abfacfac0323
Nicolas SUEUR <nsueur29@gmail.com>
2021-02-15 14:13:04 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import { VideoPlayer } from "./Components/VideoPlayer"
-import { Map } from "./Components/Map"
+import { Map } from "./Components/Map/Map"
 
 ReactDOM.render(
   <div>
